f2b463874ad19821460d1bcf9be592c4d9700116
Rasmus Pind <rasmus.pind@khk.ee>
2015-11-02 14:31:51 +0200

    Lisatud require {Delivers #104982742}

--- include/database.php
+++ include/database.php
@@ -3,3 +3,5 @@
 // Connect database
 $db = mysqli_connect('127.0.0.1', 'root', '', 'cleanblog') or die(mysqli_error($db));
 mysqli_query($db, "SET NAMES 'utf8'");
+
+require('../config.sample.php');
